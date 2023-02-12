eab64a351b43a01f80e6dc5fcff314e1e2ab1942
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2023-02-12 21:13:58 +0900

    babelの設定追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,23 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        // 処理対象ファイル
+        test: /\.js$/,
+        // 処理対象ディレクトリ
+        include: path.resolve(__dirname, 'src/js'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env', { modules: false}]]
+            }
+          }
+        ]
+      }
+    ]
   }
 };
